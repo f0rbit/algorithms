404fd0af6c12afca597541011e9483b4feed685e
f0rbit <dev@forbit.dev>
2024-04-15 07:34:54 +0930

    404 - Sum of Left Leaves

---
+++ leetcode/easy/404-sum-of-left-leaves.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        return helper(root->left, true) + helper(root->right, false);
+    }
+    int helper(TreeNode* root, bool left) {
+        if (root == nullptr) return 0;
+        if (root->left == nullptr && root->right == nullptr) {
+            return left ? root->val : 0;
+        }
+        return helper(root->left, true) + helper(root->right, false);
+    }
+};
